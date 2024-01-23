a52cf6615d43f581d83955a95e1f3dcabf9451b2
truongdang0707 <157467360+truongdang0707@users.noreply.github.com>
2024-01-23 20:33:51 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,313 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+	autoplay:true,
+	autoplayTimeout:5000,
+    nav:true,
+	
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#product-sale').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+	
+	  })
+
+$('#product-phone').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-phone-2').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-laptop').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-tablet').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-watch').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-sound').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-sound-2').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:50,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-accessory').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+		dots:false,
+
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#banner-prioritize').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	dots:false,
+	margin:10,
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#store').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:10,
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+	
+	  })
+
